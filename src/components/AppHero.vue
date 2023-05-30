5d4dceac49542aff33062a13d62e5f4ff7d3ccb0
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-30 13:36:40 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-image: url(/vite.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 600px;
+    height: 600px;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    color: #9b5cd7;
+}
+</style>
